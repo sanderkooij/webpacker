11673d82a8fa4c6140e87aa3e56599c9ffb84c3c
weareworkingonit <nickgroenewegen@relatics.com>
2019-09-14 14:14:57 +0200

    chore: upgraded deps. added more functional approach of code

--- src/helpers.js
+++ src/helpers.js
@@ -1,13 +1,23 @@
 // @flow
+type EventType = 'click' | 'submit'
+
+export function pipe(...fns: any[]) {
+  return (x: any) => fns.reduce((prev, func) => func(prev), x)
+}
+
 export function square(value: number): number {
   return value * value
 }
 
 export function pickValues(
-  obj: { [string | number ]: string | number | boolean },
-  keys: Array<string | number>,
+  obj: Object,
+  keys: Array<string>,
 ): Array<string | number | boolean> {
-  return keys.map(key => obj[key])
+  function pickValue(key) {
+    return obj[key]
+  }
+
+  return keys.map(pickValue)
 }
 
 export function fromPairs(
@@ -18,9 +28,31 @@ export function fromPairs(
 
 export function preventDefault(event: Event) {
   event.preventDefault()
+  return event
 }
 
-/* istanbul ignore next */
-export function treeshakeMe(arg: any): any {
-  return arg
+export function useEventListener(type: EventType, element: HTMLElement) {
+  function useEventListenerAttach(handler: any) {
+    element.addEventListener(type, handler)
+
+    function useEventListenerCleanUp() {
+      element.removeEventListener(type, handler)
+    }
+
+    return useEventListenerCleanUp
+  }
+
+  return useEventListenerAttach
+}
+
+export function useFormData(constructor: any): any {
+  function toIterableFormData(event: Event): FormData {
+    return new constructor(event.target)
+  }
+
+  return toIterableFormData
+}
+
+export function iterableToPairs(iterable: any) {
+  return [...iterable]
 }
--- src/helpers.test.js
+++ src/helpers.test.js
@@ -3,6 +3,7 @@ import {
   square,
   pickValues,
   fromPairs,
+  useEventListener,
   preventDefault,
 } from './helpers'
 
@@ -29,7 +30,30 @@ describe('preventDefault', () => {
   it('Should correctly call preventDefault on the passed event object', () => {
     const event = new Event('mocked')
     jest.spyOn(event, 'preventDefault')
-    preventDefault(event)
+    const result = preventDefault(event)
     expect(event.preventDefault).toHaveBeenCalled()
+    expect(result).toEqual(event)
+  })
+})
+
+describe('useEventListener', () => {
+  const element = document.createElement('a')
+  const mockedHandler = jest.fn()
+  const addEventListenerSpy = jest.spyOn(element, 'addEventListener')
+  const removeEventListenerSpy = jest.spyOn(element, 'removeEventListener')
+  const cleanUpEventListener = useEventListener('click', element)(mockedHandler)
+
+  it('Should call addEventListener with the passed handler', () => {
+    expect(addEventListenerSpy).toHaveBeenCalledWith('click', mockedHandler)
+  })
+
+  it('Should correctly call the handler when the event is fired', () => {
+    element.click()
+    expect(mockedHandler).toHaveBeenCalled()
+  })
+
+  it('Should call removeEventListener with the passed handler', () => {
+    cleanUpEventListener()
+    expect(removeEventListenerSpy).toHaveBeenCalledWith('click', mockedHandler)
   })
 })
